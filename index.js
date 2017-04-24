ce1b259f02270d311cbdcd055fdc18676d1b8828
Nicolas Galler <ngaller@gmail.com>
2017-04-24 12:53:46 -0400

    add timeout

--- index.js
+++ index.js
@@ -1,14 +1,38 @@
-const restify = require('restify')
+/**
+ * Serve fake responses for the RFI Mobile App
+ */
+
+const restify = require('restify'),
+  fs = require('fs')
 
 const server = restify.createServer({})
 server.use(restify.bodyParser({}))
 
-server.get('/Job/GetJobSummary', function(req, res, next) {
-  res.send(200, {
-    'hello': 'test'
-  });
+server.get('/Authentication/Authenticate', (req, res, next) => {
+  const {Username, Password} = req.body
+  if(Username == 'test' && Password == 'test') {
+    res.send(200, {
+    })
+  } else {
+    res.send(500, {
+      Message: 'Authentication Failed'
+    })
+  }
   return next()
 })
 
+server.get('/Job/GetJobSummary', (req, res, next) => {
+  res.send(200, "3")
+  return next()
+})
+
+server.get('/Job/GetJobs', (req, res, next) => {
+  setTimeout(() => {
+    fs.readFile('samples/GetJobs.json', (err, data) => {
+      res.send(200, data)
+    })
+  }, 6000)
+  return next()
+})
 
 server.listen(process.env.PORT)
