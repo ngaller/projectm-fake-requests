af48c1e58f08f2403048db97d884c42d36f7a9a4
Nicolas Galler <ngaller@gmail.com>
2017-04-24 11:46:38 -0400

    initial

---
+++ index.js
@@ -0,0 +1,13 @@
+const restify = require('restify')
+
+const server = restify.createServer({})
+server.use(restify.bodyParser({}))
+
+server.get('Job/GetJobSummary', function(req, res, next) {
+  res.send(200, {
+    'hello': 'test'
+  });
+  return next()
+})
+
+
