d3c2d68fa7ffd2a3a6afb3b01b7124a262111371
Nicolas Galler <ngaller@gmail.com>
2017-04-24 11:53:38 -0400

    fix url

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const restify = require('restify')
 const server = restify.createServer({})
 server.use(restify.bodyParser({}))
 
-server.get('Job/GetJobSummary', function(req, res, next) {
+server.get('/Job/GetJobSummary', function(req, res, next) {
   res.send(200, {
     'hello': 'test'
   });
